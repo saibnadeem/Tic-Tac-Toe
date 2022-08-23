9d80aeaff6943f3efee21b10abe86798b05b8ad1
saibnadeem <saqibnadeem00010@gmail.com>
2022-08-23 11:23:59 +0530

    created board square and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import React from "react";
-
-export default () => (
-  <>
-    <h1>Welcome to React Vite Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+import React from 'react';
+import { ReactDOM } from 'react';
+import Board from './components/Board.jsx';
+const App = () => {
+  return (
+    <>
+      <h1>TIC TAC TOE</h1>
+      <Board />
+    </>
+  );
+};
+export default App;
